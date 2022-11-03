f448c1a9e15635f9ebb998abd17bf6d6f9f467e3
James Bernardi <jamesbernardi@users.noreply.github.com>
2022-11-03 15:19:03 -0400

    no s in block

--- main.tf
+++ main.tf
@@ -124,8 +124,8 @@ resource "aws_security_group_rule" "public_traefik_http_ingress" {
   protocol          = "tcp"
   from_port         = var.http_port
   to_port           = var.http_port
-  cidr_blocks       = [data.aws_vpc.vpc.cidr_blocks]
-  ipv6_cidr_blocks  = [data.aws_vpc.vpc.ipv6_cidr_blocks]
+  cidr_blocks       = [data.aws_vpc.vpc.cidr_block]
+  ipv6_cidr_blocks  = [data.aws_vpc.vpc.ipv6_cidr_block]
 }
 
 resource "aws_security_group_rule" "public_traefik_https_ingress" {
@@ -135,8 +135,8 @@ resource "aws_security_group_rule" "public_traefik_https_ingress" {
   protocol          = "tcp"
   from_port         = var.https_port
   to_port           = var.https_port
-  cidr_blocks       = [data.aws_vpc.vpc.cidr_blocks]
-  ipv6_cidr_blocks  = [data.aws_vpc.vpc.ipv6_cidr_blocks]
+  cidr_blocks       = [data.aws_vpc.vpc.cidr_block]
+  ipv6_cidr_blocks  = [data.aws_vpc.vpc.ipv6_cidr_block]
 }
 
 
