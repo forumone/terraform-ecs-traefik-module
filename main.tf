c76445b3209a95f4c595c231adb2a09cfef9e0ce
James Bernardi <jamesbernardi@users.noreply.github.com>
2022-11-04 14:24:52 -0400

    []

--- main.tf
+++ main.tf
@@ -124,8 +124,8 @@ resource "aws_security_group_rule" "public_traefik_http_ingress" {
   protocol          = "tcp"
   from_port         = var.http_port
   to_port           = var.http_port
-  cidr_blocks       = var.public_subnets_ipv4
-  ipv6_cidr_blocks  = var.public_subnets_ipv6
+  cidr_blocks       = [var.public_subnets_ipv4]
+  ipv6_cidr_blocks  = [var.public_subnets_ipv6]
 }
 
 resource "aws_security_group_rule" "public_traefik_https_ingress" {
@@ -135,8 +135,8 @@ resource "aws_security_group_rule" "public_traefik_https_ingress" {
   protocol          = "tcp"
   from_port         = var.https_port
   to_port           = var.https_port
-  cidr_blocks       = var.public_subnets_ipv4
-  ipv6_cidr_blocks  = var.public_subnets_ipv6
+  cidr_blocks       = [var.public_subnets_ipv4]
+  ipv6_cidr_blocks  = [var.public_subnets_ipv6]
 }
 
 
