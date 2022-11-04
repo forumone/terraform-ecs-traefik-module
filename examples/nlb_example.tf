5f379e7293ceb6c23866e6acc1fa52832f82cdcd
Steve Huang <shuang@forumone.com>
2022-11-04 09:51:51 -0400

    Add log level to example

--- examples/nlb_example.tf
+++ examples/nlb_example.tf
@@ -1,8 +1,8 @@
 module "traefik" {
-  source      = "github.com/forumone/terraform-ecs-traefik-module"
-  version     = "~> 0.1"
-  name        = "ecs-cluster"
-  ecs_cluster = "ecs-cluster"
-  suffix      = "demo.com"
-
+  source            = "github.com/forumone/terraform-ecs-traefik-module"
+  version           = "~> 0.1"
+  name              = "ecs-cluster"
+  ecs_cluster       = "ecs-cluster"
+  suffix            = "demo.com"
+  traefik_log_level = "DEBUG"
 }
