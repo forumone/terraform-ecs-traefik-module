6565c3ed0532d9ead1bd2403f10bf26a64d5a183
James Bernardi <jamesbernardi@users.noreply.github.com>
2022-11-08 11:02:47 -0500

    set to defaults

--- variables.tf
+++ variables.tf
@@ -4,8 +4,8 @@ variable "nlb_id" {}
 variable "suffix" {}
 variable "default_acm" {}
 variable "ecs_cluster_name" {}
-variable "http_port" { default = 8000 }
-variable "https_port" { default = 8443 }
+variable "http_port" { default = 80 }
+variable "https_port" { default = 443 }
 variable "traefik_version" { default = "latest" }
 variable "traefik_log_level" { default = "ERROR" }
 variable "public_subnets_ipv4" {}
