b90a4c26efdbf5e3fa022afc1ba06d168254e831
James Bernardi <jamesbernardi@users.noreply.github.com>
2022-11-09 13:49:36 -0500

    input is a set now

--- examples/nlb_example.tf
+++ examples/nlb_example.tf
@@ -1,7 +1,5 @@
 module "traefik" {
-  source               = "github.com/forumone/terraform-ecs-traefik-module?ref=v0.2.0"
-  name                 = local.name
-  suffix               = local.workspace["suffix"]
+  source               = "github.com/forumone/terraform-ecs-traefik-module?ref=v1.0.0"
   vpc_id               = module.vpc.vpc_id
   public_subnets_ipv4  = module.vpc.public_subnets_cidr_blocks
   public_subnets_ipv6  = module.vpc.public_subnets_ipv6_cidr_blocks
